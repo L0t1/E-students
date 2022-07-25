15ce3804548e47ef18d6802ed553cf8d8c282cb7
Lot Kyqyku <lotkyqyku@gmail.com>
2022-07-25 17:54:38 +0200

    routes

--- school-management-system/src/app/app-routing.module.ts
+++ school-management-system/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
+import { MesuesiComponent } from './mesuesi/mesuesi.component';
+import { NxenesiComponent } from './nxenesi/nxenesi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",component: AppComponent},
+  {path: "admin",component: AdminComponent},
+  {path: "mesuesi",component: MesuesiComponent},
+  {path: "nxenesi",component: NxenesiComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
